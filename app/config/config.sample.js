1e58ca42e7539746fa0af873d06d7a6c17e18354
Udhaya <Udbtech@gmail.com>
2018-01-09 21:57:30 +0530

    updateee

--- app/config/config.sample.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // endpoint: "https://dove.ignorelist.com/dev/",
-    endpoint: "https://dove.ignorelist.com/pro",
-    middleWare: "/api/v1",
-    campaignId: 26174,
-    // campaignId: 26414
-}
--- update.server.sh
+++
@@ -1,5 +0,0 @@
-
-rm -rf dist;
-env NODE_ENV=production ./node_modules/.bin/webpack --config ./webpack.static.config.js --progress --profile --colors
-rm -rf dist-live;
-cp -r dist dist-live;
